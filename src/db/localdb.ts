2f01d4a59915865025663ba89281a102a42c558a
tailuge <tailuge@gmail.com>
2023-11-08 09:11:16 +0000

    promise resolve

--- src/db/localdb.ts
+++ src/db/localdb.ts
@@ -4,22 +4,16 @@ export class LocalDb implements Db {
   readonly store = new Map<string, any>()
 
   async delete(_: string, key: string): Promise<boolean> {
-    return new Promise<boolean>((res, _) => {
-      res(this.store.delete(key))
-    })
+    return Promise.resolve(this.store.delete(key))
   }
 
   async get(collection: string, key: string): Promise<DbItem> {
-    return new Promise<DbItem>((res, _) => {
-      res({ collection: collection, key: key, props: this.store.get(key) })
-    })
+    return Promise.resolve({ collection: collection, key: key, props: this.store.get(key) })
   }
 
   async set(collection: string, key: string, props: any): Promise<DbItem> {
     this.store.set(key, props)
-    return new Promise<DbItem>((res, _) => {
-      res({ collection: collection, key: key, props: props })
-    })
+    return Promise.resolve({ collection: collection, key: key, props: props })
   }
 
   async list(collection: string): Promise<DbItem[]> {
@@ -27,8 +21,6 @@ export class LocalDb implements Db {
     this.store.forEach((value, key) => {
       result.push({ collection: collection, key: key, props: value })
     })
-    return new Promise<DbItem[]>((res, _) => {
-      res(result)
-    })
+    return Promise.resolve(result)
   }
 }
