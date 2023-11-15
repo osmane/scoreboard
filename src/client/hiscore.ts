b6a418cdca4545755be0c6aeab675789c32ad11c
tailuge <tailuge@gmail.com>
2023-11-15 17:00:44 +0000

    hiscore

--- src/client/hiscore.ts
+++ src/client/hiscore.ts
@@ -1,9 +1,34 @@
 import JSONCrush from "jsoncrush"
-const a = { x: 1, y: 2 }
 
-console.log(JSON.stringify(a))
-console.log(JSONCrush.crush(JSON.stringify(a)))
+start()
 
-const embed = document.getElementById("embed")! as HTMLIFrameElement
-embed.src =
-  "https://tailuge.github.io/billiards/dist/embed.html" + location.search
+async function start() {
+  showReplay()
+  reportInfo()
+}
+
+function showReplay() {
+  const link = "https://tailuge.github.io/billiards/dist/embed.html"
+  const embed = document.getElementById("embed")! as HTMLIFrameElement
+  embed.src = link + location.search
+}
+
+function reportInfo() {
+  const params = new URLSearchParams(location.search)
+  const replay = params.get("state")!
+  const ruletype = params.get("ruletype") ?? "nineball"
+  const breakState = parse(decodeURIComponent(replay))
+  const elapsed = new Date(breakState.now - breakState.start)
+    .toISOString()
+    .substring(14, 19)
+  const info = document.getElementById("info")!
+  info.innerHTML = `${ruletype} break of ${breakState.score} in ${elapsed}`
+}
+
+function parse(s) {
+  try {
+    return JSON.parse(s)
+  } catch (_) {
+    return JSON.parse(JSONCrush.uncrush(s))
+  }
+}
