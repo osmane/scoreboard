8d105e758bf7b8e3c0c58c68229065d8cb601510
tailuge <tailuge@gmail.com>
2024-06-09 09:52:48 +0100

    logging

--- src/pages/api/hiscore.ts
+++ src/pages/api/hiscore.ts
@@ -11,13 +11,15 @@ const scoretable = new ScoreTable(kv)
 
 export default async function handler(request: NextRequest) {
   const url = request.nextUrl
+  console.log(url)
+  console.log(url.searchParams)
   const raw = url.searchParams.get("state")
   const json = JSON.parse(JSONCrush.uncrush(decodeURIComponent(raw)))
   const ruletype = url.searchParams.get("ruletype")
   const base = new Date("2024").valueOf()
   const score = json?.score + (new Date().valueOf() - base) / base
   const player = url.searchParams.get("id") || "***"
-  console.log(`adding ${ruletype} hiscore of ${score} for player ${player}`)
+  console.log(`Recieved ${ruletype} hiscore of ${score} for player ${player}`)
 
   const data = await scoretable.topTen(url.searchParams.get("ruletype"))
 
@@ -27,6 +29,7 @@ export default async function handler(request: NextRequest) {
       return urlState(rowData) === raw
     })
   ) {
+    console.log("Add hiscore")
     await scoretable.add(ruletype, score, player, url.search)
   }
 
