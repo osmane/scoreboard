88cd32afc4854b11cf29f3f17b8c4fb1668691ab
tailuge <tailuge@gmail.com>
2024-12-21 19:20:13 +0000

    ids

--- src/pages/api/rank/[id].ts
+++ src/pages/api/rank/[id].ts
@@ -11,11 +11,11 @@ const scoretable = new ScoreTable(kv)
 export default async function handler(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const ruletype = searchParams.get("ruletype")
-  const id = Number(searchParams.get("id"))
+  const id = searchParams.get("id")
 
   if (request.method === "GET") {
     const url = await scoretable.get(ruletype, id)
-    console.log(`redirecting ${ruletype} rank ${id} to ${url}`)
+    console.log(`redirecting ${ruletype} id ${id} to ${url}`)
     return new Response(null, {
       status: 302,
       headers: {
@@ -26,7 +26,7 @@ export default async function handler(request: NextRequest) {
 
   if (request.method === "PUT") {
     await scoretable.like(ruletype, id)
-    console.log(`liked ${ruletype} rank ${id}`)
+    console.log(`liked ${ruletype} id ${id}`)
   }
 
   return new Response(null, { status: 200 })
