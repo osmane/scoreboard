13a85b39e6d22a8335877e3d3a8a27a6cd8c91ef
tailuge <tailuge@gmail.com>
2024-12-29 17:05:49 +0000

    status

--- src/pages/lobby.tsx
+++ src/pages/lobby.tsx
@@ -14,7 +14,7 @@ export default function Lobby() {
   const [userName, setUserName] = useState("")
   const [tables, setTables] = useState<Table[]>([])
   const searchParams = useSearchParams()
-  const statusPage = "https://billiards.onrender.com"
+  const statusPage = "https://billiards-network.onrender.com/basic_status"
 
   const fetchTables = async () => {
     const res = await fetch("/api/tables")
