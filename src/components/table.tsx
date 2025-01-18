3f87314f1fdce61de3e0d7e806d1bc908c2b78e7
tailuge <tailuge@gmail.com>
2025-01-18 11:00:41 +0000

    improve table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -3,6 +3,55 @@ import { UserPlusIcon, EyeIcon } from "@heroicons/react/24/solid"
 import { GameUrl } from "@/utils/GameUrl"
 import { createOverlay } from "@/components/PlayModal"
 
+const TablePockets = () => (
+  <>
+    <div className="table-pocket table-pocket-top-left"></div>
+    <div className="table-pocket table-pocket-top-right"></div>
+    <div className="table-pocket table-pocket-bottom-left"></div>
+    <div className="table-pocket table-pocket-bottom-right"></div>
+    <div className="table-pocket-middle table-pocket-top"></div>
+    <div className="table-pocket-middle table-pocket-bottom"></div>
+  </>
+);
+
+const TableContent = ({ table, isCreator, onJoin, handleSpectate }) => (
+  <div className="table-content">
+    <div className="text-center">
+      <p className="table-title">{table.ruleType}</p>
+      <p className="table-players">
+        {table.creator.name} {table.players.length > 1 ? `vs ${table.players[1].name}` : "- waiting for opponent"}
+      </p>
+    </div>
+    <div className="table-actions">
+      {!isCreator && (
+        <>
+          {table.players.length < 2 && (
+            <button
+              onClick={() => onJoin(table.id)}
+              className="table-button"
+              aria-label="Join Table"
+            >
+              <UserPlusIcon className="h-5 w-5 text-white" />
+            </button>
+          )}
+          {table.players.length >= 2 && (
+            <button
+              onClick={handleSpectate}
+              className="table-button"
+              aria-label="Spectate Table"
+            >
+              <EyeIcon className="h-5 w-5 text-white" />
+              <span className="table-spectator-count">
+                {table.spectators.length}
+              </span>
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  </div>
+);
+
 export function TableItem({
   table,
   onJoin,
@@ -59,48 +108,13 @@ export function TableItem({
           <div
             className={`table-felt ${getFeltClass(table.ruleType, table.players.length, table.completed)}`}
           ></div>
-          <div className="table-content">
-            <div className="text-center">
-              <p className="table-title">{table.ruleType}</p>
-              <p className="table-players">
-                {table.creator.name} vs{" "}
-                {table.players.length > 1 ? table.players[1].name : "..."}
-              </p>
-            </div>
-            <div className="table-actions">
-              {!isCreator && (
-                <>
-                  {table.players.length < 2 && (
-                    <button
-                      onClick={() => onJoin(table.id)}
-                      className="table-button"
-                      aria-label="Join Table"
-                    >
-                      <UserPlusIcon className="h-5 w-5 text-white" />
-                    </button>
-                  )}
-                  {table.players.length >= 2 && (
-                    <button
-                      onClick={handleSpectate}
-                      className="table-button"
-                      aria-label="Spectate Table"
-                    >
-                      <EyeIcon className="h-5 w-5 text-white" />
-                      <span className="table-spectator-count">
-                        {table.spectators.length}
-                      </span>
-                    </button>
-                  )}
-                </>
-              )}
-            </div>
-          </div>
-          <div className="table-pocket table-pocket-top-left"></div>
-          <div className="table-pocket table-pocket-top-right"></div>
-          <div className="table-pocket table-pocket-bottom-left"></div>
-          <div className="table-pocket table-pocket-bottom-right"></div>
-          <div className="table-pocket-middle table-pocket-top"></div>
-          <div className="table-pocket-middle table-pocket-bottom"></div>
+          <TableContent 
+            table={table} 
+            isCreator={isCreator} 
+            onJoin={onJoin} 
+            handleSpectate={handleSpectate}
+          />
+          {table.ruleType !== 'threecushion' && <TablePockets />}
         </div>
       </div>
     </div>
